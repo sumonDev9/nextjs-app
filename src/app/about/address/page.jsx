3b0e664f70be0a92c418383528e65580d05122e4
sumonDev9 <sumonmit678@gmail.com>
2025-01-26 20:48:00 +0530

    address route create

---
+++ src/app/about/address/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Address = () => {
+    return (
+        <div>
+            this is a address
+        </div>
+    );
+};
+
+export default Address;
--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,21 @@
+"use client"
+import { useRouter } from 'next/navigation';
 import React from 'react';
 
 const About = () => {
+    const router = useRouter();
+    const isLoggedIn = true;
+    const handleCilck = () => {
+        if(isLoggedIn){
+            router.push('/about/address');
+        }else{
+            router.push('/');
+        }
+    }
     return (
         <div>
             <p>this is a about page</p>
+            <button onClick={handleCilck}>Addess</button>
         </div>
     );
 };
